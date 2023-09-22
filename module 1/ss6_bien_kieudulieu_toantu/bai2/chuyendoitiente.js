d688d3e72660ddb8a482308ec94f63b88772a69c
quochuy2501 <quochuy2001dn@gmail.com>
2023-09-22 13:59:02 +0700

    ss6_bien_kieudulieu_toantu

---
+++ module 1/ss6_bien_kieudulieu_toantu/bai2/chuyendoitiente.js
@@ -0,0 +1,5 @@
+function ChuyenDoiTienTe() {
+    let vnd = +document.getElementById("tien").value;
+    let usd=vnd*0.0000435;
+    alert(usd)
+}
---
+++ module 1/ss6_bien_kieudulieu_toantu/bai3/calculator.js
@@ -0,0 +1,24 @@
+function Cong() {
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let rs = Number(n1) + Number(n2);
+    alert(rs)
+}
+function Tru() {
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let rs = Number(n1) - Number(n2);
+    alert(rs)
+}
+function Nhan() {
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let rs = Number(n1) * Number(n2);
+    alert(rs)
+}
+function Chia() {
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let rs = Number(n1) / Number(n2);
+    alert(rs)
+}
