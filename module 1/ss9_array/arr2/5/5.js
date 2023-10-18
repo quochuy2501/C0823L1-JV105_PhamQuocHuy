7ee4bbe8891fa103b322342f2458e9ff3c7c7b5b
quochuy2501 <quochuy2001dn@gmail.com>
2023-10-18 19:56:51 +0700

    module 1

--- module 1/ss9_array/arr2/5/5.js
+++ module 1/ss9_array/arr2/5/5.js
@@ -1,11 +1,10 @@
-let str1 = prompt("Nhập chuỗi thứ nhất:");
-let str2 = prompt("Nhập chuỗi thứ hai:");
-function checkstring(str1,str2) {
+let str1 = prompt("Nhập chuỗi thứ nhất:")
+let str2 = prompt("Nhập chuỗi thứ hai:")
     if (str1 === str2) {
         alert("2 chuoi giong nhau")
     }else {
-        alert("2 chuoi k giong nhau")
+        alert("2 chuoi khong giong nhau")
     }
-}
+
 
 
--- module 1/ss9_array/arr2/6/6.js
+++ module 1/ss9_array/arr2/6/6.js
@@ -1,9 +1,9 @@
 let str = prompt(["nhập 1 mảng vào đây"])
 let arr= str.split('')
-document.writeln(arr+"<br>");
+document.writeln(arr.join("")+"<br>");
 for (let i=0; i<arr.length;i++){
     if(arr[i]==="-"){
         arr.splice(i,1,"_")
     }
 }
-document.write(arr)
+document.write(arr.join(""))
